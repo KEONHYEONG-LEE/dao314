d86a78be83dca83878cfbd51786a9cbbb01127ac
GPNR <kh1253.lee@gmail.com>
2026-03-27 19:25:04 +0900

    Update page.tsx
    Signed-off-by: GPNR <kh1253.lee@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -9,3 +9,26 @@ import CategoryNews from "../components/category-news";
 import NewsFeed from "../components/news-feed";
 import Newsletter from "../components/newsletter";
 import Footer from "../components/footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <GPNRHeader />
+      <Header />
+      <div className="container mx-auto px-4 py-6">
+        <BreakingNews />
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 mt-8">
+          <div className="lg:col-span-8">
+            <FeaturedNews />
+            <CategoryTabs />
+            <NewsFeed />
+          </div>
+          <aside className="lg:col-span-4">
+            <TrendingSidebar />
+          </aside>
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
+}
