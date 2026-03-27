516a7654b308e114c797158f9c2bd2d691e23d42
GPNR <kh1253.lee@gmail.com>
2026-03-27 19:09:09 +0900

    Update layout.tsx
    Signed-off-by: GPNR <kh1253.lee@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,2 @@
-// app/layout.tsx
-import './globals.css';
-
-export const metadata = {
-  title: 'Global Pi Newsroom',
-  description: 'News for Pi Network',
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="ko">
-      <body>{children}</body>
-    </html>
-  );
-}
+import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider";
