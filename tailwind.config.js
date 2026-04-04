6f00d29e6826c8cde342d9fdf2ec426e1d4fce6a
GPNR <kh1253.lee@gmail.com>
2026-04-04 19:21:24 +0900

    Update tailwind.config.js
    Signed-off-by: GPNR <kh1253.lee@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    // 혹시 다른 폴더에 파일이 있을 경우를 대비해 추가
+    "./*.{js,ts,jsx,tsx,mdx}", 
   ],
   theme: {
     extend: {
@@ -16,6 +18,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('tailwind-scrollbar-hide') // 이 부분을 추가하여 디자인 기능을 활성화합니다.
+    require('tailwind-scrollbar-hide')
   ],
 }
