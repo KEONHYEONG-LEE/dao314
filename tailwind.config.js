6fac8569637e473df9e07d843fa667d98bddda8d
GPNR <kh1253.lee@gmail.com>
2026-04-04 19:58:25 +0900

    Update tailwind.config.js
    Signed-off-by: GPNR <kh1253.lee@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    // 혹시 다른 폴더에 파일이 있을 경우를 대비해 추가
-    "./*.{js,ts,jsx,tsx,mdx}", 
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
