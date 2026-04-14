e302b2be5b31aa3cd659c32958d28a335a49d9ac
GPNR <kh1253.lee@gmail.com>
2026-04-14 21:58:10 +0900

    Update _app.tsx
    Signed-off-by: GPNR <kh1253.lee@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,7 @@
+import type { AppProps } from 'next/app'
+import '../globals.css' // globals.css 경로에 맞춰 수정하세요
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
 
