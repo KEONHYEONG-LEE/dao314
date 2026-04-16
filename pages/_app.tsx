ac90776a980bc162c1b208b8b562a3933a91f25d
GPNR <kh1253.lee@gmail.com>
2026-04-16 11:56:45 +0900

    Update _app.tsx
    Signed-off-by: GPNR <kh1253.lee@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import type { AppProps } from 'next/app'
-import '../globals.css' // globals.css 경로에 맞춰 수정하세요
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'next-themes';
+import '../globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Head>
+        <title>GPNR - Global Pi Newsroom</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Pi SDK 로드 */}
+        <script src="https://sdk.minepi.com/pi-sdk.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-
