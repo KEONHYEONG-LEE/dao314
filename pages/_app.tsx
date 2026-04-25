d1099f14a1ac5c62e93c5a757ef5a46ae8571b39
GPNR <kh1253.lee@gmail.com>
2026-04-25 14:43:20 +0900

    Update _app.tsx
    Signed-off-by: GPNR <kh1253.lee@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import Script from 'next/script'; // Next.js 전용 스크립트 컴포넌트
+import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 import '../globals.css';
+import { Header } from '@/components/Header'; // Header 임포트
+import { FloatingLanguageSwitcher } from '@/components/FloatingLanguageSwitcher'; // 새로 만든 컴포넌트 임포트
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +12,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>GPNR - Global Pi Newsroom</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" /> 
-        {/* 모바일에서 줌 현상 방지 설정 추가 */}
       </Head>
 
       {/* Pi SDK 로드 최적화 */}
@@ -19,7 +20,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         strategy="beforeInteractive" 
       />
 
+      {/* 1. 공통 헤더 (이제 모든 페이지에 나옵니다) */}
+      <Header />
+
+      {/* 2. 각 페이지 내용 */}
       <Component {...pageProps} />
+
+      {/* 3. 하단 고정 번역 버튼 (상단바와 충돌 없이 바닥에 표시) */}
+      <FloatingLanguageSwitcher />
+      
     </ThemeProvider>
   );
 }
