d437acea455ecbde56764e960c984d0efc1a830f
GPNR <kh1253.lee@gmail.com>
2026-04-26 17:17:32 +0900

    Update gpnr-header.tsx
    Signed-off-by: GPNR <kh1253.lee@gmail.com>

--- components/gpnr-header.tsx
+++ components/gpnr-header.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-// 이 컴포넌트는 Header.tsx와 중복되어 사용하지 않습니다.
-// 상단 중복 출력을 막기 위해 렌더링을 제거했습니다.
+/**
+ * GPNRHeader Component
+ * Header.tsx와 기능이 중복되어 현재는 사용하지 않습니다.
+ * 상단 중복 출력을 방지하기 위해 빈 요소를 반환합니다.
+ */
 export function GPNRHeader() {
   return null;
 }
